c3781f0d536977fb21cfd65e7f277a9d2042ad78
lando91 <landh2310@gmail.com>
2021-10-19 05:31:10 +0700

    Update script.js

--- lecture/c3/udp/script.js
+++ lecture/c3/udp/script.js
@@ -4,7 +4,6 @@ function base64EncodeUnicode(str) {
     utf8Bytes = encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
         return String.fromCharCode("0x" + p1);
     });
-
     return btoa(utf8Bytes);
 }
 
