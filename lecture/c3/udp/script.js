0ef4a425deb9e15ca5a32f80697f21262aed816b
lando91 <landh2310@gmail.com>
2021-10-19 05:18:22 +0700

    Update script.js

--- lecture/c3/udp/script.js
+++ lecture/c3/udp/script.js
@@ -1,3 +1,15 @@
+
+
+function base64EncodeUnicode(str) {
+    // Firstly, escape the string using encodeURIComponent to get the UTF-8 encoding of the characters,
+    // Secondly, we convert the percent encodings into raw bytes, and add it to btoa() function.
+    utf8Bytes = encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
+        return String.fromCharCode("0x" + p1);
+    });
+
+    return btoa(utf8Bytes);
+}
+
  $(document).ready(function() {
     $(window).on('load', function () {
                $.getJSON('json-data.json', function(data) {
@@ -6,7 +18,7 @@
                   $('#n1').text(jd.number1);
                   $('#n2').text(jd.number2);
                   if (window.sessionStorage) {
-                     sessionStorage.setItem("checksum", jd.checksum);
+                     sessionStorage.setItem("checksum", base64EncodeUnicode(jd.checksum));
                   }
                });
             });  
