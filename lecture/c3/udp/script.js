f6f68e6f51431cb1da8c7331458e9ade3d5e1d54
lando91 <landh2310@gmail.com>
2021-10-19 06:50:18 +0700

    Update script.js

--- lecture/c3/udp/script.js
+++ lecture/c3/udp/script.js
@@ -9,7 +9,7 @@ function base64EncodeUnicode(str) {
  $(document).ready(function() {
     $(window).on('load', function () {
                $.getJSON('json-data.json', function(data) {
-                  var n = Math.floor(Math.random() * 10);
+                  var n = Math.floor(Math.random() * 12);
                   var jd = data.bt[n];
                   $('#n1').text(jd.number1);
                   $('#n2').text(jd.number2);
