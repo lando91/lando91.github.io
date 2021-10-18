0f5a871167366395ad71c7b4d37111b38b78b41f
lando91 <landh2310@gmail.com>
2021-10-19 04:24:15 +0700

    Update script.js

--- lecture/c3/udp/script.js
+++ lecture/c3/udp/script.js
@@ -1,6 +1,7 @@
  $(document).ready(function() {
             $("#driver").click(function(event){
-               $.getJSON('json-data.json', function(jd) {
+               $.getJSON('json-data.json', function(data) {
+                var jd = data.bt[0];
                   $('#stage').html('<p> Số 1: ' + jd.number1 + '</p>');
                   $('#stage').append('<p>Số 2 : ' + jd.number2+ '</p>');
                   $('#stage').append('<p> Checksum: ' + jd.checksum+ '</p>');
