6d7bc46183871272d440d40a56b587bf37453709
lando91 <landh2310@gmail.com>
2021-10-19 04:36:12 +0700

    Update script.js

--- lecture/c3/udp/script.js
+++ lecture/c3/udp/script.js
@@ -1,7 +1,8 @@
  $(document).ready(function() {
          $("#driver").click(function(event){
                $.getJSON('json-data.json', function(data) {
-                  var jd = data.bt[0];
+                  var n = Math.floor(Math.random() * 10);
+                  var jd = data.bt[n];
                   $('#n1').text(jd.number1);
                   $('#n2').text(jd.number2);
                   $('#csvalue').text(jd.checksum);
