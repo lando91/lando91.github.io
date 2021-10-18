7347f5517f222112ff1c64f112b3a5e5be4647e4
lando91 <landh2310@gmail.com>
2021-10-19 04:32:16 +0700

    Update script.js

--- lecture/c3/udp/script.js
+++ lecture/c3/udp/script.js
@@ -2,9 +2,9 @@
          $("#driver").click(function(event){
                $.getJSON('json-data.json', function(data) {
                   var jd = data.bt[0];
-                  $('#stage').html('<p> Số 1: ' + jd.number1 + '</p>');
-                  $('#stage').append('<p>Số 2 : ' + jd.number2+ '</p>');
-                  $('#stage').append('<p> Checksum: ' + jd.checksum+ '</p>');
+                  $('#n1').text(jd.number1);
+                  $('#n2').text(jd.number2);
+                  $('#csvalue').text(jd.checksum);
                });
             });
                
