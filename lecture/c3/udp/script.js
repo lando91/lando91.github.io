c5e2ab3a166bf8fa6a8cd5876d825397dc5101fe
lando91 <landh2310@gmail.com>
2021-10-19 05:15:00 +0700

    Update script.js

--- lecture/c3/udp/script.js
+++ lecture/c3/udp/script.js
@@ -1,12 +1,13 @@
  $(document).ready(function() {
-     $("#driver").click(function(event){
+    $(window).on('load', function () {
                $.getJSON('json-data.json', function(data) {
                   var n = Math.floor(Math.random() * 10);
                   var jd = data.bt[n];
                   $('#n1').text(jd.number1);
                   $('#n2').text(jd.number2);
-                  $('#csvalue').text(jd.checksum);
+                  if (window.sessionStorage) {
+                   sessionStorage.setItem("checksum", jd.checksum);
+                  }
                });
-            });
-               
+            });  
          });
